c9e58dbcac7bd78bf941c0a037622682e7323acc
ErickLee85 <erickcrowne85@gmail.com>
2022-06-10 10:15:30 -0500

    Added JS for time widget

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -45,3 +45,36 @@ document.addEventListener("click", function(event){
         closeForm()
     }
 }, false )
+
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
+
+/**
+ * 
+ * @param {Date} date 
+ */
+function formatTime(date) {
+    const hours12 = date.getHours() % 12 || 12;
+    const minutes = date.getMinutes();
+    const isAM = date.getHours() < 12;
+
+    return `${hours12.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")} ${isAM ? "AM" : "PM"}`;
+}
+
+/**
+ * 
+ * @param {*} date 
+ */
+
+function formatDate(date) {
+    const DAY = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const MONTH = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    return `${DAY[date.getDay()]}, ${MONTH[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
+}
+
+setInterval(() => {
+    const now = new Date();
+    timeElement.textContent = formatTime(now);
+    dateElement.textContent = formatDate(now);
+}, 200);
