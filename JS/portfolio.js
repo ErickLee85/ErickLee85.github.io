b8c77f53a0e441ae562117390b7cf92f3bfb1c7a
ErickC <ErickCrowne@outlook.com>
2022-06-10 20:36:49 -0500

    update

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -100,3 +100,26 @@ function useApiData(data) {
     document.querySelector("#api_Description").innerHTML += data.explanation
     document.querySelector("#api_Date").innerHTML += data.date
 }
+
+
+const lightbox = document.getElementById("grids");
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+    image.addEventListener('click', e=> {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.src
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !==e.currentTarget) return
+    lightbox.classList.remove('active')
+})
