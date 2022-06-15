1009a73ed2fbfc8c5b8cc5291793587daaecbba4
ErickC <ErickCrowne@outlook.com>
2022-06-14 21:54:56 -0500

    update

--- JS/weather.js
+++ JS/weather.js
@@ -15,13 +15,15 @@ async function getWeather() {
 
         const response = await fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${userZip}&days=3`, options);
         const data = await response.json();
-        console.log(data["forecast"]["forecastday"][1]);
+        // console.log(data["forecast"]["forecastday"]);
         document.getElementById("city").innerText = "Weather in " + (data["location"]["name"]) + ", " + (data["location"]["region"]);
         document.getElementById("apiIMG").innerHTML = `<img src="${data.current.condition.icon}"/>`;
         document.getElementById("temp").innerText = "Current Temp: " + (data["current"]["temp_f"]) + "°"
         document.getElementById("feels-like").innerText = "Feels like: " + (data["current"]["feelslike_f"]) + "°";
+        document.getElementById("today-max").innerText = "Maximum Temp: " + (data["forecast"]["forecastday"][0]["day"]["maxtemp_f"]) + "°";
         document.getElementById("description").innerText = (data["current"]["condition"]["text"]);
         document.getElementById("humidity").innerText = "Humidity: " + (data["current"]["humidity"]) + "%";
+        document.getElementById("today-rain").innerText = "Chance of Rain: " + (data["forecast"]["forecastday"][0]["day"]["daily_will_it_rain"]) + "%";
         document.getElementById("min-temp").innerText = "Minimum Temp: " + (data["forecast"]["forecastday"][1]["day"]["mintemp_f"]) + "°";
         document.getElementById("max-temp").innerText = "Maximum Temp: " + (data["forecast"]["forecastday"][1]["day"]["maxtemp_f"]) + "°";
         document.getElementById("tomorrow").innerText = "Tomorrow: " + (data["forecast"]["forecastday"][1]["date"]);
