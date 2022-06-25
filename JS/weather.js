9884a695b5ea6cf39154b5c7e0f2b570dc41e105
ErickC <ErickCrowne@outlook.com>
2022-06-25 16:33:57 -0500

    added error handling

--- JS/weather.js
+++ JS/weather.js
@@ -2,7 +2,7 @@ async function getWeather() {
     const userZip = document.getElementById("userZipCode").value;
 
     if (!userZip) {
-        alert("Please enter a zip code.");
+        alert("Please enter a zip code or City, State.");
         return;
     }
         const options = {
@@ -13,25 +13,30 @@ async function getWeather() {
         }
         };
 
+    
         const response = await fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${userZip}&days=3`, options);
-        const data = await response.json();
-        // console.log(data["forecast"]["forecastday"][1]["day"]["condition"]["icon"]);
-        document.getElementById("city").innerText = "Weather in " + (data["location"]["name"]) + ", " + (data["location"]["region"]);
-        document.getElementById("apiIMG").innerHTML = `<img src="${data.current.condition.icon}"/>`;
-        document.getElementById("temp").innerText = "Current Temp: " + (data["current"]["temp_f"]) + "°"
-        document.getElementById("feels-like").innerText = "Feels like: " + (data["current"]["feelslike_f"]) + "°";
-        document.getElementById("today-max").innerText = "Maximum Temp: " + (data["forecast"]["forecastday"][0]["day"]["maxtemp_f"]) + "°";
-        document.getElementById("description").innerText = (data["current"]["condition"]["text"]);
-        document.getElementById("humidity").innerText = "Humidity: " + (data["current"]["humidity"]) + "%";
-        document.getElementById("today-rain").innerText = "Chance of Rain: " + (data["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain"]) + "%";
-        document.getElementById("min-temp").innerText = "Minimum Temp: " + (data["forecast"]["forecastday"][1]["day"]["mintemp_f"]) + "°";
-        document.getElementById("max-temp").innerText = "Maximum Temp: " + (data["forecast"]["forecastday"][1]["day"]["maxtemp_f"]) + "°";
-        document.getElementById("tomorrow").innerText = "Tomorrow: " + (data["forecast"]["forecastday"][1]["date"]);
-        document.getElementById("rain").innerText = "Chance of Rain: " + (data["forecast"]["forecastday"][1]["day"]["daily_chance_of_rain"]) + "%";
 
-        document.getElementById("visibility").innerText = "Visibility: " + (data["current"]["vis_miles"]) + " miles";
-        document.getElementById("wind").innerText = "Wind Speed: " + (data["current"]["wind_mph"]) + " mph";
-        document.getElementById("direction").innerText = "Wind Direction: " + (data["current"]["wind_dir"]);
-        document.getElementById("gusts").innerText = "Wind Gusts: " + (data["current"]["gust_mph"]) + " mph";
-        document.getElementById("precipitation").innerText = "Rain fall: " + (data["current"]["precip_in"]) + " in";            
+        if (!response.ok) {alert("Something went wrong, try again...");}
+
+        else {
+            const data = await response.json();
+            document.getElementById("city").innerText = "Weather in " + (data["location"]["name"]) + ", " + (data["location"]["region"]);
+            document.getElementById("apiIMG").innerHTML = `<img src="${data.current.condition.icon}"/>`;
+            document.getElementById("temp").innerText = "Current Temp: " + (data["current"]["temp_f"]) + "°"
+            document.getElementById("feels-like").innerText = "Feels like: " + (data["current"]["feelslike_f"]) + "°";
+            document.getElementById("today-max").innerText = "Maximum Temp: " + (data["forecast"]["forecastday"][0]["day"]["maxtemp_f"]) + "°";
+            document.getElementById("description").innerText = (data["current"]["condition"]["text"]);
+            document.getElementById("humidity").innerText = "Humidity: " + (data["current"]["humidity"]) + "%";
+            document.getElementById("today-rain").innerText = "Chance of Rain: " + (data["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain"]) + "%";
+            document.getElementById("min-temp").innerText = "Minimum Temp: " + (data["forecast"]["forecastday"][1]["day"]["mintemp_f"]) + "°";
+            document.getElementById("max-temp").innerText = "Maximum Temp: " + (data["forecast"]["forecastday"][1]["day"]["maxtemp_f"]) + "°";
+            document.getElementById("tomorrow").innerText = "Tomorrow: " + (data["forecast"]["forecastday"][1]["date"]);
+            document.getElementById("rain").innerText = "Chance of Rain: " + (data["forecast"]["forecastday"][1]["day"]["daily_chance_of_rain"]) + "%";
+    
+            document.getElementById("visibility").innerText = "Visibility: " + (data["current"]["vis_miles"]) + " miles";
+            document.getElementById("wind").innerText = "Wind Speed: " + (data["current"]["wind_mph"]) + " mph";
+            document.getElementById("direction").innerText = "Wind Direction: " + (data["current"]["wind_dir"]);
+            document.getElementById("gusts").innerText = "Wind Gusts: " + (data["current"]["gust_mph"]) + " mph";
+            document.getElementById("precipitation").innerText = "Rain fall: " + (data["current"]["precip_in"]) + " in";
+        } 
 }
