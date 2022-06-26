97a53d18d5c2712d696db3dcffc71b04aa3e7350
ErickC <ErickCrowne@outlook.com>
2022-06-25 19:12:47 -0500

    adding error checking

--- JS/astronomy.js
+++ JS/astronomy.js
@@ -15,19 +15,26 @@ async function getUserCity() {
     };
 
     const response = await fetch(`https://weatherapi-com.p.rapidapi.com/astronomy.json?q=${userCity}`, options);
-    const data = await response.json();
+    
+    if(!response.ok) {
+        alert("Something went wrong, please try again...");
+    }
+    else {
+        const data = await response.json();
     
 
-    document.getElementById("sunrise").innerHTML = (data["astronomy"]["astro"]["sunrise"]);
-    document.getElementById("sunset").innerHTML = (data["astronomy"]["astro"]["sunset"]);
-    document.getElementById("moonrise").innerHTML = (data["astronomy"]["astro"]["moonrise"]);
-    document.getElementById("moonset").innerHTML = (data["astronomy"]["astro"]["moonset"]);
-    document.getElementById("moonphase").innerHTML = (data["astronomy"]["astro"]["moon_phase"]);
-    document.getElementById("moonillumination").innerHTML = (data["astronomy"]["astro"]["moon_illumination"]);
-    document.getElementById("country").innerHTML = (data["location"]["country"]);
-    document.getElementById("lat").innerHTML = (data["location"]["lat"]);
-    document.getElementById("long").innerHTML = (data["location"]["lon"]);
-    document.getElementById("name").innerHTML = (data["location"]["name"]);
-    document.getElementById("region").innerHTML = (data["location"]["region"]);
-    document.getElementById("localtime").innerHTML = (data["location"]["localtime"]);        
+        document.getElementById("sunrise").innerHTML = (data["astronomy"]["astro"]["sunrise"]);
+        document.getElementById("sunset").innerHTML = (data["astronomy"]["astro"]["sunset"]);
+        document.getElementById("moonrise").innerHTML = (data["astronomy"]["astro"]["moonrise"]);
+        document.getElementById("moonset").innerHTML = (data["astronomy"]["astro"]["moonset"]);
+        document.getElementById("moonphase").innerHTML = (data["astronomy"]["astro"]["moon_phase"]);
+        document.getElementById("moonillumination").innerHTML = (data["astronomy"]["astro"]["moon_illumination"]);
+        document.getElementById("country").innerHTML = (data["location"]["country"]);
+        document.getElementById("lat").innerHTML = (data["location"]["lat"]);
+        document.getElementById("long").innerHTML = (data["location"]["lon"]);
+        document.getElementById("name").innerHTML = (data["location"]["name"]);
+        document.getElementById("region").innerHTML = (data["location"]["region"]);
+        document.getElementById("localtime").innerHTML = (data["location"]["localtime"]);     
+    }
+   
     }
